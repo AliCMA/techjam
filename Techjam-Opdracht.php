03f4e803004e15031b2ca177a6f910b7ac63f566
AliCMA <32778@ma-web.nl>
2023-09-07 13:35:48 +0200

    Opdracht

--- Techjam-Opdracht.php
+++ Techjam-Opdracht.php
@@ -1,9 +1,19 @@
 <?php
 
-
+//Hier maak ik een for loop, waardoor ik niet alles hoef te echo'en elke nummer maar dat het in een lijst gaat automatisch.
 for ($x = 33; $x <= 543; $x++) {
     echo "Nummer: $x <br>";
   }
+
+
+//Hier was ik bezig met de 2e stap, maar daar liep ik nog vast.
+  if ($x & 1) {
+    echo "<b>$x</b>";
+} elseif ($x & 2) {
+    echo "<b>$x</b>";
+} elseif ($x % 3 == 0) {
+    echo "<b>$x</b>";
+}
 
 
 ?>
